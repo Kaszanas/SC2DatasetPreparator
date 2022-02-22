7fbf0dac5b2dd5bd4c04ec5227fc5377cbeed990
Andrzej Białecki <kaszanas@interia.pl>
2022-02-23 00:40:30 +0100

    Added Rust version, defined TODOs

--- src/directory_flattener/src/main.rs
+++ src/directory_flattener/src/main.rs
@@ -1,3 +1,34 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(short, long, default_value = "./processing/directory_flattener/input")]
+    input_directory: String,
+
+    /// Number of times to greet
+    #[clap(short, long, default_value = "./processing/directory_flattener/output")]
+    output_directory: String,
+}
+
 fn main() {
-    println!("Hello");
+    let args = Args::parse();
+
+    println!(
+        "Input Something! {}! Output {}!",
+        args.input_directory, args.output_directory
+    )
+
+    // TODO: Iterate over the input directory
+
+    // TODO: If the path is a directory then go in
+
+    // TODO: Walk through the directory
+
+    // TODO: If the file extension matches the CLI extension then copy the file
+    // And save it to a directory mapping.
+
+    // Save the directory mapping to drive.
 }
