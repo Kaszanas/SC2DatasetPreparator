579d341606571592cc899c4a8702ba55acd5578c
Andrzej Białecki <kaszanas@interia.pl>
2022-02-24 22:54:54 +0100

    Drafted use of UUID in Rust impl

--- src/directory_flattener/src/main.rs
+++ src/directory_flattener/src/main.rs
@@ -1,6 +1,7 @@
-use std::str::FromStr;
+use std::{path::Path, str::FromStr};
 
 use clap::Parser;
+use uuid::Uuid;
 use walkdir::WalkDir;
 
 /// Simple program to greet a person
@@ -23,7 +24,7 @@ struct Args {
     )]
     output_directory: String,
 
-    #[clap(short, long, default_value = ".SC2Replay")]
+    #[clap(short, long, default_value = "SC2Replay")]
     file_extension: String,
 }
 
@@ -44,8 +45,13 @@ fn main() {
             continue;
         }
 
-        let extension =
-            String::from_str(entry.path().extension().unwrap().to_str().unwrap()).unwrap();
+        let os_extension = entry.path().extension();
+        if os_extension.is_none() {
+            println!("Detected that file had no extension");
+            continue;
+        }
+        let unwrap_os_extension = os_extension.unwrap();
+        let extension = String::from_str(unwrap_os_extension.to_str().unwrap()).unwrap();
 
         if extension != args.file_extension {
             println!("Detected a wrong file extension");
@@ -55,6 +61,27 @@ fn main() {
         // Find all of the files:
         if entry.file_type().is_file() && extension == args.file_extension {
             // If the file extension matches then copy the file:
+
+            let old_filename = entry
+                .path()
+                .file_stem()
+                .and_then(|stem| stem.to_str())
+                .unwrap();
+
+            let mut my_uuid = Uuid::new_v4().to_simple().to_string();
+            println!("{}", my_uuid);
+
+            println!("{}", old_filename);
+            // let old_file_no_extension = old_file_w_extension.strip_suffix(&extension).unwrap();
+
+            let mut extension_w_dot = String::from(".");
+            extension_w_dot.push_str(&args.file_extension);
+            my_uuid.push_str(&extension_w_dot);
+            let output_path = Path::new(&args.output_directory).join(my_uuid);
+            continue;
+
+            // std::fs::copy(entry.path(), to)
+
             // And save it to a directory mapping.
             // Save the directory mapping to drive.
         }
