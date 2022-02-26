c6de43afefee22bbc4a01a7030697a889c856ac4
Andrzej Białecki <kaszanas@interia.pl>
2022-02-26 02:14:03 +0100

    Added save_dir_mapping fn, refactored code

--- src/directory_flattener/src/replaypack_walker.rs
+++ src/directory_flattener/src/replaypack_walker.rs
@@ -3,13 +3,17 @@ use serde_json::{Map, Value};
 use uuid::Uuid;
 use walkdir::WalkDir;
 
-use std::{panic, path::Path, str::FromStr};
+use std::{
+    fs::File,
+    panic,
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    /// Name of the person to greet
+    /// Input directory that will be walked and searched for replaypacks
     #[clap(
         short,
         long,
@@ -17,14 +21,15 @@ pub struct Args {
     )]
     pub input_directory: String,
 
-    /// Number of times to greet
+    /// Output directory where the files will be copied into a flat directory structure
     #[clap(
         short,
         long,
-        default_value = "../processing/directory_flattener/output"
+        default_value = "F:\\Projects\\SC2DatasetPreparator\\processing\\directory_flattener\\output"
     )]
     pub output_directory: String,
 
+    /// File extension which will be used to detect the files that ought to be copied to a new flat directory structure
     #[clap(short, long, default_value = "SC2Replay")]
     pub file_extension: String,
 }
@@ -42,6 +47,7 @@ pub fn directory_flattener(args: Args) {
     for input_replaypack in test_vec {
         let mut directory_mapping = Map::new();
         let mut replaypack_input_dir = String::from("");
+        let mut output_dir_path = None;
         // Iterating over all of the files and subdirectories of a replaypack:
         for entry in WalkDir::new(input_replaypack) {
             let entry = entry.unwrap();
@@ -116,12 +122,19 @@ pub fn directory_flattener(args: Args) {
                 extension_w_dot.push_str(&args.file_extension);
                 unique_id_filename.push_str(&extension_w_dot);
 
-                let output_dir_path = Path::new(&args.output_directory).join(&replaypack_input_dir);
+                output_dir_path =
+                    Some(Path::new(&args.output_directory).join(&replaypack_input_dir));
 
-                println!("{}", output_dir_path.to_str().unwrap());
+                println!("{}", output_dir_path.to_owned().unwrap().to_str().unwrap());
 
-                let output_dir = std::fs::create_dir_all(&output_dir_path);
-                match output_dir {
+                let copy_to_path = output_dir_path
+                    .to_owned()
+                    .unwrap()
+                    .join(&unique_id_filename);
+                println!("{}", copy_to_path.to_str().unwrap());
+
+                let is_output_dir = std::fs::create_dir_all(&output_dir_path.to_owned().unwrap());
+                match is_output_dir {
                     Ok(output_path) => output_path,
                     Err(error) => panic!(
                         "Could not create the directory! Received the following error {:?}",
@@ -129,8 +142,7 @@ pub fn directory_flattener(args: Args) {
                     ),
                 };
 
-                let is_copied =
-                    std::fs::copy(entry.path(), output_dir_path.join(&unique_id_filename));
+                let is_copied = std::fs::copy(entry.path(), copy_to_path);
                 match is_copied {
                     Err(error) => panic!(
                         "Could not copy the file! The following error was raised: {:?}",
@@ -145,7 +157,24 @@ pub fn directory_flattener(args: Args) {
                 );
             }
         }
+        // Save the directory mapping to the drive
+        match output_dir_path {
+            Some(output_dir_path) => save_dir_mapping(output_dir_path, directory_mapping),
+            None => {
+                panic!("Could not create a directory mapping file! Found output_dir_path = None")
+            }
+        }
     }
+}
 
-    // iterate over all of the paths with walkdir with the alg that's already defined
+fn save_dir_mapping(output_dir_path: PathBuf, dir_mapping: Map<String, Value>) {
+    let output_json_path = &File::create(Path::new(&output_dir_path)).unwrap();
+    let did_write = serde_json::to_writer(output_json_path, &dir_mapping);
+    match did_write {
+        Err(error) => panic!(
+            "Could not write the directory mapping file! The following error was raise: {}",
+            error
+        ),
+        Ok(_) => (),
+    }
 }
