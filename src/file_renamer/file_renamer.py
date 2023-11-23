54f02a2d407082a944ca7b2373a60c49b0fbc67d
Kaszanas <andrzej.bialecki94@gmail.com>
2023-11-23 17:29:33 +0100

    Refactored file_renamer to use click

--- src/file_renamer/file_renamer.py
+++ src/file_renamer/file_renamer.py
@@ -1,18 +1,22 @@
 import os
-import argparse
+from pathlib import Path
+
+import click
 
 
-def file_renamer(input_dir: str) -> None:
+def file_renamer(input_path: Path) -> None:
     """
     Provides logic for renaming files with .zip and .json files that are contained within a directory.
     Contains hardcoded rules: if the file is of .zip extension it adds "_data" prefix to the filename.
     And if the file is of .json extension it adds "_summary" prefix to the filename.
 
     :param input_dir: Specifies the input directory where the files will be renamed.
-    :type input_dir: str
+    :type input_dir: Path
     """
-    for directory, _, file_list in os.walk(input_dir):
+    # TODO: This can be done with iterdir:
+    for directory, _, file_list in os.walk(input_path.as_posix()):
         for file in file_list:
+            # REVIEW: Can this be done better? Match case statement?
             if file.endswith(".zip"):
                 os.rename(
                     os.path.join(directory, file),
@@ -51,15 +55,19 @@ def file_renamer(input_dir: str) -> None:
                 )
 
 
+@click.command(
+    help="Tool used for processing StarCraft 2 (SC2) datasets. with https://github.com/Kaszanas/SC2InfoExtractorGo"
+)
+@click.option(
+    "--input_path",
+    type=click.Path(exists=True, dir_okay=True, file_okay=False, resolve_path=True),
+    required=True,
+    help="Please provide input path to the directory containing the dataset that is going to be processed by packaging into .zip archives.",
+)
+def main(input_path: Path) -> None:
+    # TODO: Add logging!
+    file_renamer(input_path=input_path)
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Tool used for processing StarCraft 2 (SC2) datasets with https://github.com/Kaszanas/SC2InfoExtractorGo"
-    )
-    parser.add_argument(
-        "--input_dir",
-        type=str,
-        help="Please provide input path to the directory containing the dataset that is going to be processed.",
-        default="../../processing/sc2_replaypack_processor/output",
-    )
-    args = parser.parse_args()
-    file_renamer(input_dir=args.input_dir)
+    main()
