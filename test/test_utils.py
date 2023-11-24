d8a012e16b4e461f7aa211ec0455219294294d6d
Kaszanas <andrzej.bialecki94@gmail.com>
2023-11-24 23:12:08 +0100

    Refactored test_utils, numpy docstring style

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,7 +1,6 @@
 import os
 import logging
 from pathlib import Path
-from typing import Dict
 
 TEST_DIR_NAME = "test"
 
@@ -11,10 +10,12 @@ def get_workspace_dir() -> Path:
     Getting path to the workspace where tests are run.
     This function is future proof for testing outsite of local environments.
 
-    :return: Returns string representation of the path \
-    to the workspace "ProcessingModule/"
-    :rtype: str
+    Returns
+    -------
+    Path
+        Returns the path to the workspace.
     """
+
     logging.info(
         "Entered get_workspace_dir(), attempting to set \
         workspace_dir = os.environ.get('TEST_WORKSPACE')"
@@ -27,68 +28,108 @@ def get_workspace_dir() -> Path:
     )
     return workspace_dir
 
+    # TODO: This should be creating input directory if it doesn't exist:
+    # TODO: Additionally, there should be a script that downloads test input files if they are not already available:
 
-# TODO: This should be creating input directory if it doesn't exist:
-# TODO: Additionally, there should be a script that downloads test input files if they are not already available:
+
 def get_test_input_dir(script_name: str) -> Path:
     """
     Getting path to the files required for testing.
 
-    :return: Returns a absoolute path to the test_files dir
-    :rtype: Path
+    Parameters
+    ----------
+    script_name : str
+        Script name for which the test input dir will be retrieved.
+
+    Returns
+    -------
+    Path
+        Returns an absolute path to the test input directory.
     """
-    logging.info("Entered get_input_dir(), calling workspace_dir = get_workspace_dir()")
     workspace_dir = get_workspace_dir()
     logging.info(f"Successfully set workspace_dir = {workspace_dir}")
 
-    logging.info(
-        f"Attempting to set \
-        input_dir = os.path.join(workspace_dir, '{TEST_DIR_NAME}/test_files/{script_name}/output')"
-    )
     input_dir = Path(
         workspace_dir, f"{TEST_DIR_NAME}/test_files/{script_name}/input"
     ).resolve()
-    logging.info(f"Successfully set input_dir = {input_dir}, returning input_dir")
+    logging.info(f"Successfully set input_dir = {input_dir}, returning.")
 
     return input_dir
 
 
-def create_test_output_dirs(script_name: str) -> Dict[str, str]:
+def create_test_input_dir(script_name: str) -> Path:
     """
-    Creating output directories for tests
+    Creates test input directory for tests.
 
-    :return: Returns a dictionary of output directories, \
-    example: {video_output_dir, thumbnail_output_dir}
-    :rtype: Dict
+    Parameters
+    ----------
+    script_name : str
+        Script name for which the test output dir will be created.
+
+    Returns
+    -------
+    Path
+        Returns path to the test input directory.
+    """
+
+    workspace_dir = get_workspace_dir()
+
+    input_dir = Path(
+        workspace_dir, f"{TEST_DIR_NAME}/test_files/{script_name}/input"
+    ).resolve()
+
+    if not input_dir.exists():
+        input_dir.mkdir()
+
+    return input_dir
+
+
+def create_test_output_dir(script_name: str) -> Path:
+    """
+    Creates test output directories for tests.
+
+    Parameters
+    ----------
+    script_name : str
+        Script name for which the test oput dir will be created.
+
+    Returns
+    -------
+    Path
+        Returns path to the test output directory.
     """
 
     workspace_dir = get_workspace_dir()
     logging.info(f"Successfully set workspace_dir = {workspace_dir}")
 
-    test_output_path = os.path.join(
+    test_output_path = Path(
         workspace_dir, f"{TEST_DIR_NAME}/test_files/{script_name}/output"
-    )
+    ).resolve()
 
-    if not os.path.exists(test_output_path):
+    if not test_output_path.exists():
         logging.info(
             f"Detected that output_path does not exist, \
             calling os.mkdir({test_output_path})"
         )
-        os.mkdir(test_output_path)
+        test_output_path.mkdir()
 
-    return {
-        "test_output_path": test_output_path,
-    }
+    return test_output_path
 
 
-def get_test_output_dirs(script_name: str) -> Dict[str, str]:
+def get_test_output_dir(script_name: str) -> Path:
     """
-    Getting the paths to the video output directory
+    Getting the path to the test output directory
     and to the thumbnail output directory.
 
-    :return: Paths to the video output directory and to \
-    the thumbnail output directory.
-    :rtype: Dict
+    Parameters
+    ----------
+    script_name : str
+        Script name for which the test input dir will be retrieved.
+
+    Returns
+    -------
+    Path
+        Paths to the test output directory.
     """
 
     logging.info(
@@ -98,39 +139,42 @@ def get_test_output_dirs(script_name: str) -> Dict[str, str]:
     logging.info(
         f"Successfully set workspace_dir = {workspace_dir}, returning output_dir = "
     )
-    test_output_dir = os.path.join(
+    test_output_dir = Path(
         workspace_dir, f"{TEST_DIR_NAME}/test_files/{script_name}/output"
+    ).resolve()
+
+    return test_output_dir
+
+
+def delete_test_output(script_name: str) -> None:
+    """
+    Deletes output of tests.
+
+    Parameters
+    ----------
+    script_name : str
+        Script name for which the directory will be cleaned.
+    """
+
+    test_dir = get_test_output_dir(script_name=script_name)
+    logging.info(f"Successfully set {test_dir.as_posix()=}")
+
+    if not test_dir.exists():
+        logging.info("Did not detect test_output to exist, exiting function")
+        return
+
+    logging.info(
+        f"Detected that test_output exists, \
+            performing removal by calling shutil.rmtree({test_dir})"
     )
-
-    return {"test_output_dir": test_output_dir}
-
-
-def delete_test_output():
-    """
-    Deleting output of video processing tests.
-    """
-
-    test_dirs = get_test_output_dirs()
-
-    test_output = test_dirs["test_output_dir"]
-    logging.info(f"Successfully set test_output = {test_output}")
-
-    for _, directory in test_dirs:
-        if os.path.exists(directory):
-            logging.info(
-                f"Detected that test_output exists, \
-                performing removal by calling shutil.rmtree({test_output})"
-            )
-            # shutil.rmtree(test_output)
-        else:
-            logging.info("Did not detect test_output to exist, exiting function")
-            pass
+    # shutil.rmtree(test_output)
 
 
 class AssetError(Exception):
     pass
 
 
+# TODO:This needs to take into consideration the script name.
 def dir_test_create_cleanup(func):
     """
     Decorator that is creating output directories for tests,
@@ -138,14 +182,13 @@ def dir_test_create_cleanup(func):
     """
 
     def wrapper(*args, **kwargs):
-        create_test_output_dirs()
+        create_test_output_dir()
 
         try:
             exit_value = func(*args, **kwargs)
         finally:
             logging.info("Deleting output dir in decorator is turned off")
             # delete_test_output_dir()
-
         return exit_value
 
     return wrapper
