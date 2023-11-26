3c603c9ee394e60dc500b60192ab54f6d25a44df
Kaszanas <andrzej.bialecki94@gmail.com>
2023-11-27 00:16:26 +0100

    Refactored json_merger

--- src/sc2datasetpreparator/json_merger/json_merger.py
+++ src/sc2datasetpreparator/json_merger/json_merger.py
@@ -5,15 +5,15 @@ from typing import Dict
 import click
 
 
-def merge_files(path_to_json_one: str, path_to_json_two: str) -> Dict[str, str]:
+def merge_files(path_to_json_one: Path, path_to_json_two: Path) -> Dict[str, str]:
     """
     Exposes the logic to merge two json files by loading their contents from supplied paths.
 
     Parameters
     ----------
-    path_to_json_one : str
+    path_to_json_one : Path
         Specifies the filepath of a .json file that is going to be merged.
-    path_to_json_two : str
+    path_to_json_two : Path
         Specifies the filepath of a .json file that is going to be merged.
 
     Returns
@@ -23,11 +23,11 @@ def merge_files(path_to_json_one: str, path_to_json_two: str) -> Dict[str, str]:
     """
 
     # Loading the supplied .json files and deserializing them:
-    json_one_file = open(path_to_json_one, encoding="utf-8")
+    json_one_file = path_to_json_one.open(encoding="utf-8")
     loaded_json_one = json.loads(json_one_file.read())
     json_one_file.close()
 
-    json_two_file = open(path_to_json_two, encoding="utf-8")
+    json_two_file = path_to_json_two.open(encoding="utf-8")
     loaded_json_two = json.loads(json_two_file.read())
     json_two_file.close()
 
@@ -37,28 +37,56 @@ def merge_files(path_to_json_one: str, path_to_json_two: str) -> Dict[str, str]:
     return result_dict
 
 
-def save_output(output_filepath: str, output_dict: Dict[str, str]) -> None:
+def save_output(output_filepath: Path, output_dict: Dict[str, str]) -> Path:
     """
     Exposes the logic for saving a dict to a .json file.
 
     Parameters
     ----------
-    output_filepath : str
+    output_filepath : Path
         Speciifies the full output filepath which will be used, \
         this includes the filename.
     output_dict : Dict[str, str]
         Specifies the Python dictionary which will be serialized into a JSON.
+
+    Returns
+    -------
+    Path
+        Returns a path to the saved file.
     """
 
-    with open(output_filepath, "w") as output_file:
+    with output_filepath.open(mode="w", encoding="utf-8") as output_file:
         json.dump(output_dict, output_file, indent=4)
+
+    return output_filepath
 
 
 def json_merger(
     path_to_json_one: Path, path_to_json_two: Path, output_filepath: Path
-) -> None:
+) -> Path:
+    """
+    Merges two JSON files into one.
+
+    Parameters
+    ----------
+    path_to_json_one : Path
+        Path to first JSON file.
+    path_to_json_two : Path
+        Path to second JSON file.
+    output_filepath : Path
+        Filepath which will contain the final output of the merging.
+
+    Returns
+    -------
+    Path
+        Returns a path to the saved merged file.
+    """
+
     merge_files(path_to_json_one=path_to_json_one, path_to_json_two=path_to_json_two)
-    save_output(output_filepath=output_filepath)
+
+    final_out_filepath = save_output(output_filepath=output_filepath)
+
+    return final_out_filepath
 
 
 @click.command(
