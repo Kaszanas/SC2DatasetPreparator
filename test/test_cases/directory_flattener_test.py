95eb4193f6a5f28c8fe0231c8d114de3534121c0
Kaszanas <andrzej.bialecki94@gmail.com>
2023-12-06 17:30:31 +0100

    directory_flattener_test ready for testing

--- test/test_cases/directory_flattener_test.py
+++ test/test_cases/directory_flattener_test.py
@@ -1,6 +1,6 @@
 import unittest
 from sc2datasetpreparator.directory_flattener.directory_flattener import (
-    directory_flattener,
+    multiple_directory_flattener,
 )
 
 from test.test_utils import (
@@ -23,22 +23,52 @@ class DirectoryFlattenerTest(unittest.TestCase):
         cls.file_extension = ".SC2Replay"
 
         # Create nested directory structure mimicking how real replaypacks look like:
-        nested_dirs = create_nested_test_directories(input_path=cls.input_path)
+        cls.n_dirs = 2
+        nested_dirs = create_nested_test_directories(
+            input_path=cls.input_path, n_dirs=cls.n_dirs
+        )
+        cls.n_nested_files = 4
+        cls.n_out_of_distribution_files = 1
         for directory in nested_dirs:
-            # Create multiple test files with the selected extension:.
+            # Create multiple test files with the selected extension:
             create_test_text_files(
-                input_path=directory, n_files=4, extension=cls.file_extension
+                input_path=directory,
+                n_files=cls.n_nested_files,
+                extension=cls.file_extension,
             )
-            # Create additional files that should not be present in the output:,
-            create_test_text_files(input_path=directory, n_files=1, extension=".txt")
+            # Create additional files that should not be present in the output:
+            create_test_text_files(
+                input_path=directory,
+                n_files=cls.n_out_of_distribution_files,
+                extension=".txt",
+            )
 
     def test_directory_flattener(self) -> None:
-        directory_flattener(
+        ok, list_of_output_dirs = multiple_directory_flattener(
             input_path=self.input_path,
             output_path=self.output_path,
             file_extension=self.file_extension,
         )
-        # TODO: Assert the final directory not to contain any other nested directories.
-        # TODO: Assert the final directory have the same number of files with the selected extension.
-        # TODO: Assert the final flattened directory to have one .json file
-        # containing mapping of the files.
+
+        # Check if the input data was correct for processing:
+        self.assertTrue(ok)
+
+        # Check the number of output directories:
+        self.assertEqual(self.n_dirs, len(list_of_output_dirs))
+
+        for output_dir in list_of_output_dirs:
+            # Assert the final directory have the same number of
+            # files with the selected extension.
+            out_files = output_dir.glob(f"*{self.file_extension}")
+            self.assertEqual(self.n_nested_files, len(out_files))
+
+            # Assert the final flattened directory to
+            # have one .json file with the mapping
+            json_file = output_dir.glob(f"*{self.file_extension}")
+            self.assertEqual(1, len(json_file))
+
+            # There should be no nested directories in the output:
+            for file in output_dir.iterdir():
+                self.assertFalse(file.is_dir())
+
+            # TODO: Check the contents of the json mapping?
